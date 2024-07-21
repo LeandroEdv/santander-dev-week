527a556862dec7eb3ed5da02b15dddead9de0e0a
leandro Santana <leandro.edv@hotmail.com>
2024-07-21 16:15:27 -0300

    valores constates em java

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -2,11 +2,14 @@ public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salario = 2500f;
         System.out.println(salario);
-// ---------------------------------------------------------------
+    // ---------------------------------------------------------------
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; // casting - conversão de valores!
+        
+    // --------------------------------------------------------------------   
+        final double VALOR_PI = 3.14; // valor constante com a palavra reservada "final"
         
 
 
