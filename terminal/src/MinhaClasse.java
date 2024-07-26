e23e20da94075bba4f214c83dd750770d7a546e6
leandro Santana <leandro.edv@hotmail.com>
2024-07-26 15:16:21 -0300

    estudo de execução no terminal

---
+++ terminal/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        String nome = args [0];
+        int idade = Integer.valueOf(args[1]);
+
+        System.out.println("meu nome é: "+ nome +"minha idade é: "+ idade);
+    }
+}
