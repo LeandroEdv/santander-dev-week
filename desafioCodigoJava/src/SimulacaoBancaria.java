7eca3f8c4e4c593b7f02ade73711d2c864f81e13
leandro Santana <leandro.edv@hotmail.com>
2024-07-29 17:57:22 -0300

    desafios de codigo java

---
+++ desafioCodigoJava/src/SimulacaoBancaria.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    System.out.println("Quanto deseja depositar ?");
+                    saldo = scanner.nextDouble();
+                    System.out.println("Saldo atual: "+ saldo);
+                    break;
+                case 2:
+                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    double saque = scanner.nextDouble();
+                    if (saldo> saque) {
+                        saldo -= saque;
+                        System.out.println("Saldo atual: "+ saldo);
+                    }else{
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: "+ saldo);
+                    // TODO: Exibir o saldo atual da conta.
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
