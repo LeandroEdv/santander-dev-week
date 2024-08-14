e8e754c440c8cf27473ef02fcea30e9284ccfa67
leandro Santana <leandro.edv@hotmail.com>
2024-08-14 09:58:54 -0300

    Implementação da camada de dominio

---
+++ src/main/java/dio/dimain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dio.dimain.repository;
+
+import dio.dimain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
